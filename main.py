fc1efebd94ee62d840141de6af799162bad20221
unknown <ekeinchiaemmanuela@gmail.com>
2022-05-16 04:21:09 -0700

    output interpretation

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ weight = int(input("Enter your weight in kg: "))
 
 # BMI calculation
 bmi = round(weight / height ** 2, 2)
+
+# BMI interpretation based on BMI value
+if bmi < 18.5 :
+    print(f"your BMI is {bmi}, you are underweight.")
+elif bmi < 25 :
+    print(f"your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30 :
+    print(f"your BMI is {bmi}, you are overweight.")
+elif bmi < 35:
+    print(f"your BMI is {bmi}, you are obese.")
+else:
+    print(f"your BMI is {bmi}, you are clinically obesed")
